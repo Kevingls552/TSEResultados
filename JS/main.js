cab51b6faaaf44858721cb93dc73caa1b9996cce
Kevin Sanchez <kevsanchezsanchez0@gmail.com>
2019-08-29 22:29:24 -0600

    init

---
+++ JS/main.js
@@ -0,0 +1,51 @@
+function Consultar() {
+
+    var _TIPOELECCION = $("#TIPOELECCION").val();
+    var _DEP = $("#DEP").val();
+    var _MUN = $("#MUN").val();
+  
+    $.post("https://ws2v.tse.org.gt/api/tse/resultados", {
+      PROCESO: "201902",
+      TIPOELECCION: _TIPOELECCION,
+      DEP: _DEP,
+      MUN: '0'
+    },
+      function (data, status) {
+console.log(data.data["0"].V1);
+
+        var ctx = document.getElementById('myChart');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['UNE', 'VAMOS'],
+                datasets: [{
+                    label: 'Resultados de Votacion',
+                    data: [data.data["0"].V2,data.data["0"].V1],
+                    backgroundColor: [
+                        'green',
+                        'blue'                      
+                    ],
+                    borderColor: [
+                        'green',
+                        'blue'                       
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+      }
+  
+    );
+  }
+  $(document).ready(function(){
+      Consultar();
+  }) 
